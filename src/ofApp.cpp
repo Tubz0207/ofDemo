6bbadb771feb42deb99112e51580f6f575862738
Tubz0207 <Tubz0207@user.noreply.github.com>
2017-02-15 15:08:48 -0400

    New Things

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,7 +2,7 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+	std::cout << "Hello from setup .\n";
 }
 
 //--------------------------------------------------------------
@@ -15,13 +15,45 @@ void ofApp::draw(){
 ofBackground(0);
 ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
 
-for(int i = 0; i < 3; i++)
+/*for(int i = 0; i < 3; i++)
  {
-   ofRotate(20);
-   ofDrawLine(0,0,0,220);
-
- }
+   ofRotate(50);
+   ofSetColor(255,0,0);
+   ofDrawLine(10,0,0,220);
+   ofDrawLine(10-100,0,0,220);
+   ofDrawLine(10+100,0,0,220);
+  }
+   ofSetColor(0,0,255);
+   ofNoFill();
+   ofDrawCircle(0,0,100);
+for(int j = 0; j < 1000; j++)
+{
+	ofRotate(50);
+	ofSetColor(0,255,0);
+	ofDrawLine(100,0,0,220);
+}*/
+for(int i= 0; i < 250; i++)
+{
+	for(int j = 0; j < 200; j++)
+	{
+		ofSetColor(0,0,255);
+		ofNoFill();
+		ofDrawCircle(0,0,10);
+		ofRotate(20);
+		ofSetColor(255,0,0);
+		ofDrawLine(10,10,220,220);
+	}
+	ofSetColor(0,255,0);
+	ofNoFill();
+	ofDrawCircle(220,220,10);
+	ofRotate(10);
+	ofSetColor(10,255,0);
+	ofDrawLine(230,230,230,400);
+	ofDrawLine(230-100,230,230,400);
+	ofDrawLine(230+100,230,230,400);
 }
+}
+
 
 
 
